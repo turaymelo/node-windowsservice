2e0bdca204451b81b5a5e9a55226c0200dcd2a14
Turay Melo <turaymelo@gmail.com>
2021-06-13 12:14:35 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var Service = require('node-windows').Service;
+
+// Create a new service object
+var svc = new Service({
+    name:'AttendanceApi',
+    description: 'The nodejs.org example web server.',
+    script: 'C:\\home\\sheu\\attendance\\backend\\dist\\server.js'    
+  });
+
+
+// Listen for the "install" event, which indicates the
+// process is available as a service.
+svc.on('install',function(){
+    console.log('install init');
+  
+    svc.start();
+    // console.log('The service exists: ',svc.exists);
+    console.log('install started ');
+  });
+  
+  svc.install();
+  console.log('install complete.');
